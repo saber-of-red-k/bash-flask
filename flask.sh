91905fdda009f08134d3ce9d4f3abd5cf06d782c
saber-of-red-k <98237668+saber-of-red-k@users.noreply.github.com>
2022-05-12 18:39:38 +0300

    Update flask.sh

--- flask.sh
+++ flask.sh
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 @app.route('/flask_test')
 def flask_test():
-    return 'This is Flask testing. 1234 Good'
+    return 'This is Flask testing. 1234 Good\n'
 EOF
 
 export FLASK_APP=flask_app.py
